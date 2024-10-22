be83b7b2f82c9c471dd45473328e22cdd010dae0
Venkat <vke.1743@gmail.com>
2024-10-22 10:40:24 +0530

    common folder db.py

---
+++ college/common/__init__.py
@@ -0,0 +1,2 @@
+from .db import get_db, Base, engine, SessionLocal
+
---
+++ college/common/db.py
@@ -0,0 +1,26 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+CONNECTION_STRING = os.environ["DB_CONNECTION_STRING"]
+
+engine = create_engine(CONNECTION_STRING, 
+                       connect_args={
+                           "check_same_thread":False
+                       },
+                       echo=True)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(bind=engine, 
+                       autoflush=False,
+                       autocommit=False)
+
+def get_db():
+    db = SessionLocal()
+    
+    try:
+        yield db
+    
+    finally:
+        db.close()
